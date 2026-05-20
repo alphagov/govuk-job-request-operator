5e10c6d32cd645cc7dac0cede323a6323ada4c36
Sam Simpson <sam@simpson.id>
2026-05-20 14:54:48 +0100

    Define JobRequestReview resource

---
+++ api/v1/jobrequestreview_types.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// JobRequestReviewSpec defines the desired state of JobRequestReview
+type JobRequestReviewSpec struct {
+	// Name of the JobRequest resource being reviewed.
+	JobRequestName string `json:"jobRequestName"`
+	// +kubebuilder:validation:Enum=Approved;Rejected
+	Decision string `json:"decision"`
+	// A description of the review decision.
+	// +optional
+	Description string `json:"description,omitempty"`
+}
+
+// JobRequestReviewStatus defines the observed state of JobRequestReview.
+type JobRequestReviewStatus struct {
+	// Kubernetes username of the reviewer.
+	ReviewedBy string `json:"reviewedBy,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:shortName=jrr
+// +kubebuilder:subresource:status
+
+// JobRequestReview is the Schema for the jobrequestreviews API
+type JobRequestReview struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// metadata is a standard object metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitzero"`
+
+	// spec defines the desired state of JobRequestReview
+	// +required
+	Spec JobRequestReviewSpec `json:"spec"`
+
+	// status defines the observed state of JobRequestReview
+	// +optional
+	Status JobRequestReviewStatus `json:"status,omitzero"`
+}
+
+// +kubebuilder:object:root=true
+
+// JobRequestReviewList contains a list of JobRequestReview
+type JobRequestReviewList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitzero"`
+	Items           []JobRequestReview `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&JobRequestReview{}, &JobRequestReviewList{})
+}
