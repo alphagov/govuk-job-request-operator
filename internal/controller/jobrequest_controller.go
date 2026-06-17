5e4a8079555abe27e6f5b1dd5c256e47d439baeb
Jaskaran Sarkaria <jaskaran.sarkaria@digital.cabinet-office.gov.uk>
2026-06-17 17:16:22 +0100

    test: 💍 first pass working

--- internal/controller/jobrequest_controller.go
+++ internal/controller/jobrequest_controller.go
@@ -18,8 +18,11 @@ package controller
 
 import (
 	"context"
+	// "maps"
 
 	appsv1 "k8s.io/api/apps/v1"
+	batch "k8s.io/api/batch/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	platformv1 "github.com/alphagov/govuk-job-request-operator/api/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -34,6 +37,35 @@ type JobRequestReconciler struct {
 	CacheClient     client.Client
 	ApiServerClient client.Reader
 	Scheme          *runtime.Scheme
+}
+
+func (r *JobRequestReconciler) CreateJobTemplate(resource *appsv1.Deployment) (*batch.Job, error) {
+	// We want job names for a given nominal start time to have a deterministic name to avoid the same job being created twice
+	// name := fmt.Sprintf("%s-%d", cronJob.Name, scheduledTime.Unix())
+
+	jobTemplatePodSpec := *resource.Spec.Template.DeepCopy()
+	jobTemplatePodSpec.Spec.RestartPolicy = "Never"
+
+	job := &batch.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels:      make(map[string]string),
+			Annotations: make(map[string]string),
+			Name:        resource.Name,
+			Namespace:   resource.Namespace,
+		},
+		Spec: batch.JobSpec{
+			Template: jobTemplatePodSpec,
+		},
+	}
+
+	// maps.Copy(job.Annotations, resource.Spec)
+	// job.Annotations[scheduledTimeAnnotation] = scheduledTime.Format(time.RFC3339)
+	// maps.Copy(job.Labels, resource.Spec.JobTemplate.Labels)
+	if err := ctrl.SetControllerReference(resource, job, r.Scheme); err != nil {
+		return nil, err
+	}
+
+	return job, nil
 }
 
 // +kubebuilder:rbac:groups=platform.publishing.service.gov.uk,resources=jobrequests,verbs=get;list;watch;create;update;patch;delete
@@ -67,9 +99,6 @@ func (r *JobRequestReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 		return ctrl.Result{}, err
 	}
 
-	// Retrieve the resource  from the jobRequest
-	// appName := jobRequest.Spec.ContainerFrom.PodSpecFrom.Name
-
 	/*
 		Create a Job off from the jobrequest:
 		Retrieving the deployment
@@ -79,12 +108,30 @@ func (r *JobRequestReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 
 	deploymentList := &appsv1.DeploymentList{} // TODO: this could be another resource like another Job
 	opts := []client.ListOption{
-		client.MatchingFields{"metadata.name": "whitehall-admin"},
+		client.MatchingFields{"metadata.name": req.Name},
 	}
 
 	if err := r.ApiServerClient.List(ctx, deploymentList, opts...); err != nil {
-		log.Error(err, "Failed to list Resources") // TODO: this validation should be done in the cli / gatekeeper
+		log.Error(err, "Failed to list Resources") // TODO: this validation should be done in the cli / gatekeeper because a deployment that doesn't exist can't be used to create a job
 		return ctrl.Result{}, nil
+	}
+
+	jobTemplate, err := r.CreateJobTemplate(&deploymentList.Items[0])
+	if err != nil {
+		log.Error(err, "Failed to create Job Template")
+		return ctrl.Result{}, nil
+	}
+
+	createJobErr := r.CacheClient.Create(ctx, jobTemplate)
+	if createJobErr != nil {
+		log.Error(createJobErr, "Failed to create Job resource")
+		return ctrl.Result{}, createJobErr
+	}
+
+	jobList := &batch.JobList{}
+	if err := r.ApiServerClient.List(ctx, jobList, opts...); err != nil {
+		log.Error(err, "Failed to list Resources")
+		return ctrl.Result{}, err
 	}
 
 	return ctrl.Result{}, nil
