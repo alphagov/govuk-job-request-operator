913ba5a6ec3581163a755cec35d46903b0c07d0d
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2026-06-23 09:14:42 +0100

    Setup reconciliation logic for jobrequestreview controller

--- internal/controller/jobrequestreview_controller.go
+++ internal/controller/jobrequestreview_controller.go
@@ -18,7 +18,9 @@ package controller
 
 import (
 	"context"
+	"time"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -37,19 +39,56 @@ type JobRequestReviewReconciler struct {
 // +kubebuilder:rbac:groups=platform.publishing.service.gov.uk,resources=jobrequestreviews/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=platform.publishing.service.gov.uk,resources=jobrequestreviews/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the JobRequestReview object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.23.3/pkg/reconcile
 func (r *JobRequestReviewReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = logf.FromContext(ctx)
+	log := logf.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// Attempt to retrieve JobRequestReview object and if not stop reconcile
+	jobRequestReview := &platformv1.JobRequestReview{}
+	err := r.Get(ctx, req.NamespacedName, jobRequestReview)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			// If the custom resource is not found then it usually means that it was deleted or not created
+			// In this way, we will stop the reconciliation
+			log.Info("JobRequestReview resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object
+		log.Error(err, "Failed to get JobRequestReview")
+		return ctrl.Result{}, nil
+	}
+
+	// Attempt to retrieve JobRequest object and if not stop reconcile and add something to the CRD to indicate a failure
+	jobRequest := &platformv1.JobRequest{}
+	// Compose namespace with jobRequest.Name instead of req.NamespacedName
+	err = r.Get(ctx, req.NamespacedName, jobRequest)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			// If the custom resource is not found then it usually means that it was deleted or not created
+			// In this way, we will stop the reconciliation
+			log.Error(err, "JobRequest resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object
+		log.Error(err, "Failed to get JobRequest")
+		return ctrl.Result{}, nil
+	}
+
+	// If state is nil then reschedule as jobRequest object isn't setup yet
+	if jobRequest.Status.State == "" {
+		// Requeue the reconcile after certain time
+		return ctrl.Result{RequeueAfter: time.Minute}, nil
+	}
+
+	// If state is failed then stop reconcile and add something to the JobRequestReview to indicate a failure
+	if jobRequest.Status.State == "Failed" {
+		// Create an error and log it
+		log.Error(err, "JobRequest is in a Failed state so can't approve")
+		return ctrl.Result{}, nil
+	}
+
+	jobRequest.Status.State = jobRequestReview.Spec.Decision
+	// Do we need to recover the object again before flushing the state?
+	r.Status().Update(ctx, jobRequest)
 
 	return ctrl.Result{}, nil
 }
